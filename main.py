87327a1aa2a2ddad0eba2b0c7f31a4e77aa4910b
{caoyz} <28099773+CaoYunzhou@users.noreply.github.com>
2023-09-08 22:52:21 +0800

    缩短查询时间

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_sess_key():
                     'password': password,
                     'prompt': 'login',
                 }
-                time.sleep(10)
+                time.sleep(5)
                 resp = requests.post(
                     'https://ai.fakeopen.com/auth/platform/login', data=data)
                 result = {}  # Initialize the result dictionary
