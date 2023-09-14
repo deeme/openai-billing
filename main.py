a770c3d1b12126655e1d6b181646a269e068ce41
Miku <28099773+CaoYunzhou@users.noreply.github.com>
2023-09-14 18:55:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_sess_key():
                     'password': password,
                     'prompt': 'login',
                 }
-                time.sleep(5)
+                # time.sleep(5)
                 resp = requests.post(
                     'https://ai.fakeopen.com/auth/platform/login', data=data)
                 result = {}  # Initialize the result dictionary
